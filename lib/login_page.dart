6ffab33b655064c95ff67b0b584dbfa43bb190b4
Poramest  Noosri <6410110307@psu.ac.th>
2024-05-27 11:37:49 +0700

    first commit

---
+++ lib/login_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          //Hello agin
+          Text('data')
+
+          //email textfield
+
+          //password textfield
+
+          //sign in button
+
+          //not a member? register now
+        ],
+      ),
+    );
+  }
+}
